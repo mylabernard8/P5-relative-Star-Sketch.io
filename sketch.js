20c144a418d06eca3fdf08b136ac995ec9c34170
mylabernard8 <myla.bernard26@avenues.org>
2024-10-13 21:55:04 -0400

    @mrjloswald-avenues Updated sketch to include variables and to make more efficient

--- sketch.js
+++ sketch.js
@@ -4,22 +4,41 @@ function setup() {
 
 function draw() {
   background(220);
-  
+
+  // Variables\
+  let centerX = 0.5 * width;
+  let centerY = 0.5 * height;
+
+  let topPointY = 0.1 * height;
+  let upperY = 0.35 * height;
+  let lowerY = 0.55 * height;
+  let bottomPointY = 0.9 * height;
+
+  let outerRightX = 0.9 * width;
+  let innerRightX = 0.68 * width;
+  let upperRightX = 0.61 * width;
+  let bottomRightX = 0.78 * width;
+
+  let outerLeftX = 0.1 * width;
+  let innerLeftX = 0.32 * width;
+  let upperLeftX = 0.39 * width;
+  let bottomLeftX = 0.22 * width;
+
   fill(255, 255, 0);
   stroke(0);
 
   beginShape();
 
-  vertex(0.5 * width, 0.1 * height);      // Top point
-  vertex(0.61 * width, 0.35 * height);    // Upper-right
-  vertex(0.9 * width, 0.35 * height);     // Bottom-right
-  vertex(0.68 * width, 0.55 * height);    // Bottom-inner-right
-  vertex(0.78 * width, 0.9 * height);     // Bottom-most point
-  vertex(0.5 * width, 0.7 * height);      // Bottom-center
-  vertex(0.22 * width, 0.9 * height);     // Bottom-most-left
-  vertex(0.32 * width, 0.55 * height);    // Bottom-inner-left
-  vertex(0.1 * width, 0.35 * height);     // Bottom-left
-  vertex(0.39 * width, 0.35 * height);    // Upper-left
+  vertex(centerX, topPointY);         // Top point
+  vertex(upperRightX, upperY);        // Upper-right
+  vertex(outerRightX, upperY);        // Bottom-right
+  vertex(innerRightX, lowerY);        // Bottom-inner-right
+  vertex(bottomRightX, bottomPointY); // Bottom-most point
+  vertex(centerX, 0.7 * height);      // Bottom-center
+  vertex(bottomLeftX, bottomPointY);  // Bottom-most-left
+  vertex(innerLeftX, lowerY);         // Bottom-inner-left
+  vertex(outerLeftX, upperY);         // Bottom-left
+  vertex(upperLeftX, upperY);         // Upper-left
 
   endShape(CLOSE);
 }
